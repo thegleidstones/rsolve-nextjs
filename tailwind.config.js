3e5738ffcd849bca4dd4186d7688a3eab993ceea
Gleidson Peres <silvamorais.ti@gmail.com>
2023-08-11 15:46:18 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const windmill = require('@roketid/windmill-react-ui/config')
 module.exports = windmill({
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './containers/**/*.{js,ts,jsx,tsx}', './example/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'lime': {
+        100: '#ecfccb',
+        200: '#d9f99d',
+        300: '#bef264',
+        400: '#a3e635',
+        500: '#84cc16',
+        600: '#65a30d',
+        700: '#4d7c0f',
+        800: '#3f6212',
+        900: '#365314',
+      },
+    },
+  },
   extend: {},
   plugins: [],
 })
