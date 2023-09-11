440adb7992e2ff9344bdce8c18228156d08f6edf
Gleidson Peres <silvamorais.ti@gmail.com>
2023-09-11 15:20:28 -0300

    other itens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,23 @@ import 'tailwindcss/tailwind.css';
 import React from 'react'
 import { Windmill } from '@roketid/windmill-react-ui'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from 'context/AuthContext';
+import { SelectedGrievanceProvider } from 'hooks/GrievanceTreatment/useSelectedGrievance';
+import GrievanceTreatmentConsulting from './app/grievanceTreatmentConsulting';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // suppress useLayoutEffect warnings when running outside a browser
   if (!process.browser) React.useLayoutEffect = React.useEffect;
 
   return (
-    <Windmill usePreferences={true}>
-      <Component {...pageProps} />
-    </Windmill>
+    <AuthProvider>
+      <SelectedGrievanceProvider>
+        <Windmill usePreferences={true}>
+          <Component {...pageProps} />
+            {/* <GrievanceTreatmentConsulting /> */}
+        </Windmill>
+      </SelectedGrievanceProvider>
+    </AuthProvider>
   )
 }
 export default MyApp
