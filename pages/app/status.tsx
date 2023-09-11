3a0c616a22586e0843cb5276d432995514a04628
Gleidson Peres <silvamorais.ti@gmail.com>
2023-09-11 14:55:11 -0300

    create status component

--- pages/app/status.tsx
+++ pages/app/status.tsx
@@ -1,80 +1,140 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-import { Input, Label, Button } from '@roketid/windmill-react-ui'
-import PageTitle from 'example/components/Typography/PageTitle'
-import SectionTitle from 'example/components/Typography/SectionTitle'
+import { Input, Label, HelperText } from '@roketid/windmill-react-ui';
+import PageTitle from 'example/components/Typography/PageTitle';
+import SectionTitle from 'example/components/Typography/SectionTitle';
 
-import Layout from 'example/containers/Layout'
+import Layout from 'example/containers/Layout';
+import ActionButtonGroup from 'components/ActionsButtonGroup/ActionBruttonsGroup';
+import ModalResolve from 'components/Modal/ModalRsolve';
+import TableRsolve from 'components/Table/TableRsolve';
+
+import { useTables } from 'hooks/Table/useTable';
+import { useModals } from 'hooks/Modal/useModals';
+import { useStatuses } from 'hooks/Status/useStatuses';
+
+
+type Status = {
+  id: string;
+  name: string;
+  companyId: string;
+}
 
 function Status() {
-    const [formValues, setFormValues] = useState({
-    name: "",
+  const { statuses, formValues, setFormValues, handleSubmit, handleInactivate, handleChange } = useStatuses();
+  const { isModalOpen, isDeleteModalOpen, modal, openModal, closeModal, openDeleteModal, closeDeleteModal } = useModals();
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const router = useRouter();
+
+  // setup pages control for every table
+  const [pageTable, setPageTable] = useState(1);
+
+  // pagination setup
+  const resultsPerPage = 5;
+  const totalResults = statuses.length;
+
+  const {
+    displayedcategories: displayedTablecategories,
+    // ... outras funções e estados do useTables
+  } = useTables({
+    data: statuses,
+    pageTable,
+    resultsPerPage,
   });
 
-  const handleSubmit = async (e: any) => {
+  function openEditForm(reason: Status) {
+    setFormValues(reason); // Preenche o formulário com os dados do registro selecionado
+  }
+
+  // pagination change control
+  function onPageChangeTable(p: number) {
+    setPageTable(p);
+  }
+
+  function handleSave(e: any) {
     e.preventDefault();
-
-    try {
-      const response = await fetch("http://localhost:3344/statuses", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formValues),
-      });
-
-      if (response.ok) {
-        // Success, do something
-        console.log("Status created successfully!");
-      } else {
-        // Handle error
-        console.error("Error creating status");
-      }
-    } catch (error) {
-      console.error("Error:", error);
+    if (isFormValid) {
+      handleSubmit(e);
+      openModal();
     }
-  };
+  }
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
-  };
+  useEffect(() => {
+    setIsFormValid(formValues.name.trim() !== '' && formValues.name.length >= 4);
+  }, [formValues.name]);
 
   return (
     <Layout>
-      <PageTitle>Status da Denúncia</PageTitle> 
+      <PageTitle>Status de Denúncias</PageTitle>
       <SectionTitle>Cadastro de Status</SectionTitle>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
           <Label>
-            <span>Status</span>
-            <Input           
-             name="name"
-             value={formValues.name}
-             onChange={handleChange} 
-             className="mt-1" 
-             placeholder="Informe o status no processo de cadastro" 
-            /> 
-          </Label>           
+            <span>nome</span>
+            <Input
+              name="name"
+              value={formValues.name}
+              onChange={handleChange}
+              className="mt-1"
+              placeholder="Informe o status do processo"
+            />
+            {!isFormValid && (
+              <HelperText>O nome do status deve ter pelo menos 4 caracteres.</HelperText>
+            )}
+          </Label>
 
-            <div className="flex flex-col flex-wrap space-y-4 md:flex-row md:items-end md:space-x-4 px-4 py-3 mb-6">
-            {/* <div className="flex flex-col flex-wrap mb-8 space-y-4 md:flex-row md:items-end md:space-x-4"> */}
-              <div>
-                <Button
-                  type="submit" 
-                  className="bg-lime-600 hover:bg-lime-500" 
-                  size="larger"
-                >
-                  Registrar
-                </Button>
-              </div>
-              <div>
-                <Button className="bg-red-700 hover:bg-red-600" size="larger">Cancelar</Button>
-              </div>
-            </div>
-        </div>      
-      </form> 
+          <div className="flex flex-col flex-wrap space-y-4 md:flex-row md:items-end md:space-x-4 px-4 py-3 mb-6">
+          </div>
 
+          <ActionButtonGroup
+            isFormValid={isFormValid}
+            onSave={handleSave}
+            onCancel={() => {
+              router.push('/app/home'); // Redirecionamento para a rota /app/home
+            }}
+          />
+        </div>
+      </form>
+
+      <TableRsolve
+        data={statuses}
+        columns={[
+          { label: 'ID', key: 'id' },
+          { label: 'Name', key: 'name' },
+        ]}
+        currentPage={pageTable}
+        totalResults={totalResults}
+        resultsPerPage={resultsPerPage}
+        onPageChangeTable={onPageChangeTable}
+        onEdit={openEditForm}
+        onDelete={openDeleteModal}
+      />
+
+      {isModalOpen && (
+        <ModalResolve
+          modalHeader="Cadastro de Status"
+          modalBody="Status de denúncia cadastradas com sucesso!"
+          onClose={closeModal}
+          successMessage={true}
+        />
+      )};
+
+      {isDeleteModalOpen && (
+        <ModalResolve
+          modalHeader="Excluir status"
+          modalBody={`Deseja realmente excluir o status: ${modal?.name}?`}
+          onClose={closeDeleteModal}
+          onConfirm={() => {
+            if (modal) {
+              handleInactivate(modal.id);
+            }
+            closeDeleteModal();
+          }}
+          successMessage={false}
+        />
+      )}
     </Layout>
   )
 }
