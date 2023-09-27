d4ed3199cb95fe560b6d557c3d6441c5207a7a89
Gleidson Peres <silvamorais.ti@gmail.com>
2023-09-27 20:43:33 -0300

    create count by witness data to charts

--- hooks/Charts/useCharts.ts
+++ hooks/Charts/useCharts.ts
@@ -1,5 +1,6 @@
 import { fetchData } from "hooks/FetchData/fecthData"
 import { useEffect, useState } from "react"
+import { amberColors, blueColors, cyanColors, greenColors, newColorGenerator, orangeColors, redColors, tealColors, yellowColors } from "utils/colors";
 
 type GrievanceCompany = {
   company: string;
@@ -8,6 +9,11 @@ type GrievanceCompany = {
 
 type GrievanceDepartment = {
   department: string;
+  total: number;
+}
+
+type GrievanceWitness = {
+  witness: string;
   total: number;
 }
 
@@ -36,23 +42,28 @@ export function useCharts() {
   const [grievancesAnonymity, setGrievancesAnonymity] = useState<GrievanceAnonymity[]>([]);
   const [grievancesCompany, setGrievancesCompany] = useState<GrievanceCompany[]>([]);
   const [grievancesDepartment, setGrievancesDepartment] = useState<GrievanceDepartment[]>([]);
-  const [grievancesStatus, setGrievancesStatus] = useState<GrievanceStatus[]>([]);
   const [grievancesReason, setGrievancesReason] = useState<GrievanceReason[]>([]);
+  const [grievancesStatus, setGrievancesStatus] = useState<GrievanceStatus[]>([]);
+  const [grievancesWitness, setGrievancesWitness] = useState<GrievanceWitness[]>([]);
   const [anonymity, setAnonymity] = useState<string[]>([]);
   const [company, setCompany] = useState<string[]>([]);
   const [department, setDepartment] = useState<string[]>([]);
-  const [status, setStatus] = useState<string[]>([]);
   const [reason, setReason] = useState<string[]>([]);
+  const [status, setStatus] = useState<string[]>([]);
+  const [witness, setWitness] = useState<string[]>([]);
   const [countByAnonymity, setCountByAnonymity] = useState<number[]>([]);
   const [countByCompany, setCountByCompany] = useState<number[]>([]);
   const [countByDepartment, setCountByDepartment] = useState<number[]>([]);
   const [countByReason, setCountByReason] = useState<number[]>([]);
   const [countByStatus, setCountByStatus] = useState<number[]>([]);
+  const [countByWitness, setCountByWitness] = useState<number[]>([]);
   const [colorByAnonymity, setColorByAnonymity] = useState<string[]>([]);
   const [colorByCompany, setColorByCompany] = useState<string[]>([]);
   const [colorByDepartment, setColorByDepartment] = useState<string[]>([]);
-  const [colorByStatus, setColorByStatus] = useState<string[]>([]);
   const [colorByReason, setColorByReason] = useState<string[]>([]);
+  const [colorByStatus, setColorByStatus] = useState<string[]>([]);
+  const [colorByWitness, setColorByWitness] = useState<string[]>([]);
+
 
   // Buscando dados da API
   useEffect(() => {
@@ -68,16 +79,16 @@ export function useCharts() {
   }, []);
 
   useEffect(() => {
-    fetchData("grievances/view/count/is-anonymous", setGrievancesDepartment, "");
-  }, []);
+    fetchData("grievances/view/count/reason", setGrievancesReason, "");
+  }, [])
 
   useEffect(() => {
     fetchData("grievances/view/count/status", setGrievancesStatus, "");
   }, []);
 
   useEffect(() => {
-    fetchData("grievances/view/count/reason", setGrievancesReason, "");
-  }, [])
+    fetchData("grievances/view/count/witness", setGrievancesWitness, "");
+  }, []);
 
   // Atualizo os dados vindos de Grievances, 
   // e os separo em três vetores, para preencher os dados do gráfico
@@ -85,53 +96,63 @@ export function useCharts() {
     const newAnonymity: string[] = [];
     const newCompany: string[] = [];
     const newDepartment: string[] = [];
-    const newStatus: string[] = [];
     const newReason: string[] = [];
+    const newStatus: string[] = [];
+    const newWitness: string[] = [];
     const newCountByAnonymity: number[] = [];
     const newCountByCompany: number[] = [];
     const newCountByDepartment: number[] = [];
-    const newCountByStatus: number[] = [];
     const newCountByReason: number[] = [];
+    const newCountByStatus: number[] = [];
+    const newCountByWitness: number[] = [];
     let newColorByAnonymity: string[] = [];
     let newColorByCompany: string[] = [];
     let newColorByDepartment: string[] = [];
-    let newColorByStatus: string[] = [];
     let newColorByReason: string[] = [];
+    let newColorByStatus: string[] = [];
+    let newColorByWitness: string[] = [];
 
     grievancesAnonymity.forEach((grievanceAnonymity) => {
       newAnonymity.push(grievanceAnonymity.anonymity);
       newCountByAnonymity.push(grievanceAnonymity.total);
     })
 
-    newColorByAnonymity = colorGenerator(newAnonymity);
+    newColorByAnonymity = newColorGenerator(newAnonymity, tealColors);
 
     grievancesCompany.forEach((grievanceCompany) => {
       newCompany.push(grievanceCompany.company);
       newCountByCompany.push(grievanceCompany.total);
     });
 
-    newColorByCompany = colorGenerator(newCompany);
+    newColorByCompany = newColorGenerator(newCompany, greenColors);
 
     grievancesDepartment.forEach((grievanceDepartment) => {
       newDepartment.push(grievanceDepartment.department);
       newCountByDepartment.push(grievanceDepartment.total);
     });
 
-    newColorByDepartment = colorGenerator(newDepartment);
+    newColorByDepartment = newColorGenerator(newDepartment, yellowColors);
 
     grievancesReason.forEach((grievanceReason) => {
       newReason.push(grievanceReason.reason);
       newCountByReason.push(grievanceReason.total);
     });
 
-    newColorByReason = colorGenerator(newReason);
+    newColorByReason = newColorGenerator(newReason, redColors);
 
     grievancesStatus.forEach((grievanceStatus) => {
       newStatus.push(grievanceStatus.status);
       newCountByStatus.push(grievanceStatus.total);
     });
 
-    newColorByStatus = colorGenerator(newStatus);
+    newColorByStatus = newColorGenerator(newStatus, orangeColors);
+
+    grievancesWitness.forEach((grievanceWitness) => {
+      newWitness.push(grievanceWitness.witness);
+      newCountByWitness.push(grievanceWitness.total);
+    });
+
+    newColorByWitness = newColorGenerator(newWitness, blueColors);
 
     setAnonymity(newAnonymity);
     setCountByAnonymity(newCountByAnonymity);
@@ -142,13 +163,16 @@ export function useCharts() {
     setDepartment(newDepartment);
     setCountByDepartment(newCountByDepartment);
     setColorByDepartment(newColorByDepartment);
-    setStatus(newStatus);
-    setCountByStatus(newCountByStatus);
-    setColorByStatus(newColorByStatus);
     setReason(newReason);
     setCountByReason(newCountByReason);
     setColorByReason(newColorByReason);
-  }, [grievancesAnonymity, grievancesCompany, grievancesDepartment, grievancesReason, grievancesStatus]);
+    setStatus(newStatus);
+    setCountByStatus(newCountByStatus);
+    setColorByStatus(newColorByStatus);
+    setWitness(newWitness);
+    setCountByWitness(newCountByWitness);
+    setColorByWitness(newColorByWitness);
+  }, [grievancesAnonymity, grievancesCompany, grievancesDepartment, grievancesReason, grievancesStatus, grievancesWitness]);
 
 
   // Criando as legendas para o gráfico de doughnut (rosquinha)
@@ -156,91 +180,6 @@ export function useCharts() {
   //   title: item.status,
   //   color: getColorBasedOnStatus(item.status),
   // }));
-
-  // Função para obter a cor com base no status (apenas um exemplo, você pode personalizar isso)
-  // function getColorBasedOnStatus(status: string) {
-  //   switch (status) {
-  //     case 'Recebidas':
-  //       return 'bg-blue-500';
-  //     case 'Em Tratamento':
-  //       return 'bg-teal-600';
-  //     case 'Finalizadas':
-  //       return 'bg-lime-600';
-  //     case 'Pendentes':
-  //       return 'bg-red-600';      
-  //     case 'Improcedentes':
-  //       return 'bg-purple-600';      
-  //     default:
-  //       return 'bg-gray-500';
-  //   }
-  // }
-
-  function colorGenerator(values: string[]) {
-    const colorArray: string[] = [];
-
-    // Função para gerar uma cor aleatória em formato hexadecimal
-    function randomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-
-    for (let i = 0; i < values.length; i++) {
-      colorArray[i] = randomColor();
-    }
-
-    return colorArray;
-  }
-
-  function colorGeneratorV2(values: string[], baseColor: string): string[] {
-    const generatedColors: string[] = [];
-
-    // Obtenha os componentes RGB da cor base
-    const baseRed = parseInt(baseColor.slice(1, 3), 16);
-    const baseGreen = parseInt(baseColor.slice(3, 5), 16);
-    const baseBlue = parseInt(baseColor.slice(5, 7), 16);
-
-    values.forEach((value) => {
-      // Gere variações dos componentes RGB da cor base
-      const r = Math.floor(Math.random() * 128) + baseRed;
-      const g = Math.floor(Math.random() * 128) + baseGreen;
-      const b = Math.floor(Math.random() * 128) + baseBlue;
-
-      // Garanta que os valores RGB não ultrapassem 255
-      const randomR = Math.min(255, r);
-      const randomG = Math.min(255, g);
-      const randomB = Math.min(255, b);
-
-      // Converta os componentes RGB para uma cor hexadecimal
-      const randomColor = `#${randomR.toString(16)}${randomG.toString(16)}${randomB.toString(16)}`;
-
-      // Adicione a cor gerada ao vetor
-      generatedColors.push(randomColor);
-    });
-
-    return generatedColors;
-  }
-
-  // Função para obter a cor com base no status (apenas um exemplo, você pode personalizar isso)
-  function getColorBasedOnStatusV2(status: string) {
-    switch (status) {
-      case 'Recebido':
-        return '#3b82f6';
-      case 'Em Tratamento':
-        return '#0ea5e9';
-      case 'Finalizado':
-        return '#84cc16';
-      case 'Pendente':
-        return '#dc2626';
-      case 'Improcedente':
-        return '#9333ea';
-      default:
-        return '#6b7280';
-    }
-  }
 
   // const doughnutLegendsOLD: ILegends[] = [
   //   { title: 'Shirts', color: 'bg-blue-500' },
@@ -266,7 +205,7 @@ export function useCharts() {
            * These colors come from Tailwind CSS palette
            * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
            */
-          backgroundColor: colorByCompany,
+          backgroundColor: colorByAnonymity,
           label: 'Reclamações por Anonimidade',
         },
       ],
@@ -329,6 +268,31 @@ export function useCharts() {
     },
   }
 
+  const doughnutOptionsReason = {
+    data: {
+      datasets: [
+        {
+          data: countByReason,
+          /**
+           * These colors come from Tailwind CSS palette
+           * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+           */
+          backgroundColor: colorByReason,
+          label: 'Reclamações por motivos',
+        },
+      ],
+      labels: reason,
+    },
+    options: {
+      responsive: true,
+      cutoutPercentage: 80,
+    },
+    legend: {
+      display: true,
+    },
+  }
+
+
   const doughnutOptionsStatus = {
     data: {
       datasets: [
@@ -353,20 +317,21 @@ export function useCharts() {
     },
   }
 
-  const doughnutOptionsReason = {
+
+  const doughnutOptionsWitness = {
     data: {
       datasets: [
         {
-          data: countByReason,
+          data: countByWitness,
           /**
            * These colors come from Tailwind CSS palette
            * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
            */
-          backgroundColor: colorByReason,
-          label: 'Reclamações por motivos',
+          backgroundColor: colorByWitness,
+          label: 'Reclamações por status',
         },
       ],
-      labels: reason,
+      labels: witness,
     },
     options: {
       responsive: true,
@@ -471,6 +436,7 @@ export function useCharts() {
     doughnutOptionsDepartment,
     doughnutOptionsReason,
     doughnutOptionsStatus,
+    doughnutOptionsWitness
   }
 }
 
