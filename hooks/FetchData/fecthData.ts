27a44d2af0dca37ad17e62760b618a3c71866d64
Gleidson Peres <silvamorais.ti@gmail.com>
2024-11-07 10:39:56 -0300

    create method to fetch data from backend

--- hooks/FetchData/fecthData.ts
+++ hooks/FetchData/fecthData.ts
@@ -1,11 +1,27 @@
-  export async function fetchData(data: string, setData: any, param: string | undefined) {
-    try {
-      const response = await fetch(`http://localhost:3344/${data}/${param}`);
-      const all = await response.json();
-      // console.log("/************** logs de grievances agora, nesse testes *************************/");
-      // console.log(all);
-      setData(all);
-    } catch (error) {
-      console.error(`Error fetching ${data}:`, error);
-    }
+import { signOut } from "context/AuthContext";
+import { api } from "services/api";
+
+/** fetchData using axios api */
+export async function fetchDataStatus(data: string, setData: any, param: string | undefined) {
+  api.get(`${data}/${param}`).then(response => {
+    console.log(response.data);
+    setData(response.data);
+  }).catch(error => {
+    console.error("Passou aki nesse CATCH ERROR?")
+    console.error(error);
+    signOut();
+  });
+}
+
+/** fetchData using fetch javascript */
+export async function fetchData(data: string, setData: any, param: string | undefined) {
+  try {
+    const response = await fetch(`http://localhost:3344/${data}/${param}`);
+    const all = await response.json();
+    // console.log("/************** logs de grievances agora, nesse testes *************************/");
+    // console.log(all);
+    setData(all);
+  } catch (error) {
+    console.error(`Error fetching ${data}:`, error);
   }
+}
